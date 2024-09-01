554c1180313b3e94c88131e07e7e6d2064df051a
Victor Fernandez <germanfernandezblanco@gmail.com>
2024-09-01 22:08:08 +0200

    Add files via upload

---
+++ js/animationMenuScroll.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.header__link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+  
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
